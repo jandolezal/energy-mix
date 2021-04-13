1173032fdfb7ab4bd6d0071dcdbdb78dff5c7cc0
Jan Doležal <jandolezal@email.cz>
2021-04-13 15:49:07 +0200

    Reorganize main module

--- bot/main.py
+++ bot/main.py
@@ -14,26 +14,29 @@ import entsoe
 load_dotenv()
 
 
-# Twitter app authentication
-consumer_key = os.getenv('CONSUMER_KEY')
-consumer_secret = os.getenv('CONSUMER_SECRET')
-access_token = os.getenv('ACCESS_TOKEN')
-access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-api = tweepy.API(auth)
-
-# Get the data
+# Request electricity production data from Entsoe
 data = entsoe.request_data(entsoe.ENTSOE_URL, entsoe.ENTSOE_PARAMS)
-production = entsoe.parse_xml(data, entsoe.ENTSOE_SOURCE_MAPPING)
-grouped_production = entsoe.group_production(production)
-reordered_production = entsoe.reorder_production(grouped_production)
 
-# Make a string from emojis
-percentages = emojis.calculate_percentages(reordered_production)
-tweet = emojis.prepare_tweet(percentages)
+if data:
+    production = entsoe.parse_xml(data, entsoe.ENTSOE_SOURCE_MAPPING)
+    grouped_production = entsoe.group_production(production)
+    reordered_production = entsoe.reorder_production(grouped_production)
 
-# Tweet the data
-api.update_status(status=tweet)
+    # Make a string from emojis
+    percentages = emojis.calculate_percentages(reordered_production)
+    tweet = emojis.prepare_tweet(percentages)
+
+if tweet:
+    # Twitter app authentication and setup
+    consumer_key = os.getenv('CONSUMER_KEY')
+    consumer_secret = os.getenv('CONSUMER_SECRET')
+    access_token = os.getenv('ACCESS_TOKEN')
+    access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    api = tweepy.API(auth)
+
+    # Tweet the emoji string
+    api.update_status(status=tweet)
