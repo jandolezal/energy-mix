dc3bbadabd837627816e3567dc87c59599607752
Jan Doležal <jandolezal@email.cz>
2021-07-08 17:54:06 +0200

    Add tweet preparation test

--- tests/test_emojis.py
+++ tests/test_emojis.py
@@ -41,6 +41,28 @@ def test_calculate_percentages_better_18_hour():
     assert hour_percentages == hour_18_percentages
 
 
+def test_prepare_tweet_18_hour():
+    hour_18_percentages = {
+        'uhli': 40, 'plyn': 14, 'ropa': 0, 'jadro': 33, 'slunce': 3, 'vitr': 2,
+        'voda': 2, 'biomasa': 3, 'odpad': 0, 'ostatni_oze': 3
+        }
+    
+    tweet = (
+        '🏭🏭🏭🏭🏭🏭🏭🏭🏭🏭\n'
+        '🏭🏭🏭🏭🏭🏭🏭🏭🏭🏭\n'
+        '🏭🏭🏭🏭🏭🏭🏭🏭🏭🏭\n'
+        '🏭🏭🏭🏭🏭🏭🏭🏭🏭🏭\n'
+        '🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥\n'
+        '🔥🔥🔥🔥☢️☢️☢️☢️☢️☢️\n'
+        '☢️☢️☢️☢️☢️☢️☢️☢️☢️☢️\n'
+        '☢️☢️☢️☢️☢️☢️☢️☢️☢️☢️\n'
+        '☢️☢️☢️☢️☢️☢️☢️☀️☀️☀️\n'
+        '💨💨💧💧🌿🌿🌿♻️♻️♻️'
+    )
+
+    assert tweet == emojis.prepare_tweet(production=hour_18_percentages)
+
+
 # Case with 99 total when rounding percentages to whole numbers
 def test_calculate_percentages_better_12_hour():
     hour_12_production = {'uhli': 3345.0, 'plyn': 1166.0, 'ropa': 0.0, 'jadro': 2829.0,
@@ -78,3 +100,25 @@ def test_calculate_percentages_better_8_hour():
 
     hour_percentages = emojis.calculate_percentages_better(hour_8_production)
     assert hour_percentages == hour_8_percentages
+
+
+def test_prepare_tweet_8_hour():
+    hour_8_percentages = {
+        'uhli': 42, 'plyn': 15, 'ropa': 0, 'jadro': 31, 'slunce': 2, 'vitr': 2,
+        'voda': 2, 'biomasa': 3, 'odpad': 0, 'ostatni_oze': 3,
+        }
+    
+    tweet = (
+        '🏭🏭🏭🏭🏭🏭🏭🏭🏭🏭\n'
+        '🏭🏭🏭🏭🏭🏭🏭🏭🏭🏭\n'
+        '🏭🏭🏭🏭🏭🏭🏭🏭🏭🏭\n'
+        '🏭🏭🏭🏭🏭🏭🏭🏭🏭🏭\n'
+        '🏭🏭🔥🔥🔥🔥🔥🔥🔥🔥\n'
+        '🔥🔥🔥🔥🔥🔥🔥☢️☢️☢️\n'
+        '☢️☢️☢️☢️☢️☢️☢️☢️☢️☢️\n'
+        '☢️☢️☢️☢️☢️☢️☢️☢️☢️☢️\n'
+        '☢️☢️☢️☢️☢️☢️☢️☢️☀️☀️\n'
+        '💨💨💧💧🌿🌿🌿♻️♻️♻️'
+    )
+
+    assert tweet == emojis.prepare_tweet(production=hour_8_percentages)
